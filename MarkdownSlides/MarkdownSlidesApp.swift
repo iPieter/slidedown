8123c221e4eb609dd73de69629efd0b863badf58
Pieter Delobelle <pieter.delobelle@gmail.com>
2025-08-20 09:00:00 +0200

    feat: initial project setup

---
+++ MarkdownSlides/MarkdownSlidesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarkdownSlidesApp.swift
+//  MarkdownSlides
+//
+//  Created by Pieter Delobelle on 30/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct MarkdownSlidesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MarkdownSlidesTests/MarkdownSlidesTests.swift
@@ -0,0 +1,16 @@
+//
+//  MarkdownSlidesTests.swift
+//  MarkdownSlidesTests
+//
+//  Created by Pieter Delobelle on 30/04/2025.
+//
+
+import Testing
+
+struct MarkdownSlidesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
